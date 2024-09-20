99539fc0c27eeff7219e2618c2a395fd759a664b
Benyamin <benyamin@Benyamins-Air.fritz.box>
2024-09-20 20:47:41 +0200

    basename fix

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import AppContainer from './containers/AppContainer';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <AppContainer basename="portfoilo" />
+    <AppContainer basename="portfolio" />
 );
